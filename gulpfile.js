630caa43b3437c15a15cf37bbde884befa6cf3ca
rbalicki2 <robert.balicki@gmail.com>
2015-12-09 18:54:27 -0500

    Step 4 - pipe buffer

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,78 @@
-// Step 3
-// Goal: Create a small, useful logger plugin! If you're not sure what
-// files are being picked up by gulp.src, this is a small utility you can
-// use to inspect your pipes!
+// Step 4
+// Goal: Create a pipe utility that waits until all files have finished
+// before emitting all of the files, if and only if none have emitted an
+// error.
 
 var gulp = require('gulp'),
     through2 = require('through2');
 
-gulp.task('default', function () {
+gulp.task('no-error', function () {
   gulp.src(['src/*.txt'])
-    .pipe(logger())
+    .pipe(hooverDam())
     .pipe(gulp.dest('dest'));
 });
 
+gulp.task('error', function () {
+  gulp.src(['src/*.txt'])
+    .pipe(throwError())
+    .pipe(hooverDam())
+    .pipe(gulp.dest('dest'));
+});
 
+// hoover dam because it blocks the stream! HAR HAR HAR HAR HAR HAR
+function hooverDam () {
+  var files = [],
+      error = false;
 
-function logger () {
-  return through2.obj(function (file, enc, done) {
+  var stream = through2.obj(singleFileEncountered, allFilesCompleted);
 
-    console.log('Logger: encountered file: ' + file.path);
-    console.log('First line of file:');
-    console.log(file.contents.toString().split('\n')[0]);
-    console.log();
+  // use stream.once to not cause an infinite loop
+  stream.once('pipe', function (source) {
+    // uncaught errors will result in a thrown exception, but we don't want that.
+    // so, we can unpipe the stream, add an error handler, and then repipe it
+    console.log('The stream was piped');
 
-    this.push(file);
-    done();
+    source.unpipe(stream);
+    source.on('error', errorHandler);
+    source.pipe(stream);
+
   });
+
+  return stream;
+
+  /////////////
+
+  function errorHandler (err) {
+    console.log('Encountered an error!', err);
+    error = true;
+    stream.end();
+  }
+
+  function singleFileEncountered (file, enc, done) {
+    files.push(file);
+    done();
+  }
+
+  function allFilesCompleted (done) {
+    var fileStream = this;
+
+    if (error) {
+      console.log('Done, but error encountered in the past, so no files are pushed on');
+    } else {
+      console.log('Done, no error encountered in the past, files are being flushed'); 
+      files.forEach(fileStream.push.bind(fileStream));
+    }
+
+    if (done) {
+      done();
+    }
+  }
+}
+
+function throwError () {
+  var stream = through2.obj(function (file, enc, done) {
+    done(new Error('You tried to chew gum and walk at the same time and fell in a ditch'));
+  });
+
+  return stream;
 }
