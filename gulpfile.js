b46328ca208c3b866174e15c29a975af6248767c
rbalicki2 <robert.balicki@gmail.com>
2015-12-09 17:54:06 -0500

    Step3 - logger

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
-// step 2
-// goal: Append some text to existing files!
+// Step 3
+// Goal: Create a small, useful logger plugin! If you're not sure what
+// files are being picked up by gulp.src, this is a small utility you can
+// use to inspect your pipes!
 
 var gulp = require('gulp'),
     through2 = require('through2');
 
 gulp.task('default', function () {
   gulp.src(['src/*.txt'])
-    .pipe(myGulpPlugin())
-    .pipe(myGulpPlugin()) // do it twice to see what happens!
+    .pipe(logger())
     .pipe(gulp.dest('dest'));
 });
 
 
 
-function myGulpPlugin () {
+function logger () {
   return through2.obj(function (file, enc, done) {
 
-    var contents = file.contents.toString();
-    file.contents = new Buffer(contents + ' <- appended some stuff');
+    console.log('Logger: encountered file: ' + file.path);
+    console.log('First line of file:');
+    console.log(file.contents.toString().split('\n')[0]);
+    console.log();
 
     this.push(file);
     done();
   });
 }
-
-// NOTE we cannot pass the same stream twice to pipe! Do not do it!
