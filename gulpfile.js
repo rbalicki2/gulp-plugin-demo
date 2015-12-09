c5b3389662618658403cd9f2e17cd0d52f6cfd13
rbalicki2 <robert.balicki@gmail.com>
2015-12-09 17:49:17 -0500

    step2 - append stuff to file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
-// demo 1
-// goal set up a basic gulp file that writes from src
-// and writes to dest
+// step 2
+// goal: Append some text to existing files!
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    through2 = require('through2');
 
 gulp.task('default', function () {
   gulp.src(['src/*.txt'])
+    .pipe(myGulpPlugin())
+    .pipe(myGulpPlugin()) // do it twice to see what happens!
     .pipe(gulp.dest('dest'));
 });
+
+
+
+function myGulpPlugin () {
+  return through2.obj(function (file, enc, done) {
+
+    var contents = file.contents.toString();
+    file.contents = new Buffer(contents + ' <- appended some stuff');
+
+    this.push(file);
+    done();
+  });
+}
+
+// NOTE we cannot pass the same stream twice to pipe! Do not do it!
